b837ee3cdf7d9427b64c9cf42e4c317c61d7a4b8
Chris Hintz <chrishintz42@gmail.com>
2015-12-14 22:08:32 -0800

    App up, still not working

--- calculator.js
+++ calculator.js
@@ -1,3 +1,32 @@
-window.onload = function () {
+var calculate = function (firstNum, operator, secNum) {
+  if (operator =="+") {
+    answer = x + y;
+  } else if (operator == "-") {
+    answer = x - y;
+  } else if (operator == "x") {
+    answer = x * y;
+  } else if (operator == "/") {
+    answer = x / y;
+  }
+  return answer;
+};
 
+window.onload = function() {
+  var calculator = document.getElementById("calculator");
+  var answerDiv  = document.getElementById("answer");
+
+  calculator.addEventListener("click", function (event){
+  event.preventDefault();
+  var x         = parseFloat(document.getElementById("firstNum").value);
+  var y         = parseFloat(document.getElementById("secNum").value);
+  var operator  = document.getElementById("operator").value;
+
+  var answer    = calculate(x, operator, y);
+  calculator.elements.x.value         = answer;
+  answerDiv.innerHTML                 = answer;
+  });
+
+  select.addEventListener("change", function (){
+
+  });
 };
