36f104adc1ece5abad819a88e2289501b1e62fd8
Chris Hintz <chrishintz42@gmail.com>
2015-12-14 23:23:18 -0800

    Calculator working, clear button not

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
 var calculate = function (firstNum, operator, secNum) {
   if (operator =="+") {
-    answer = x + y;
+    answer = firstNum + secNum;
   } else if (operator == "-") {
-    answer = x - y;
+    answer = firstNum - secNum;
   } else if (operator == "x") {
-    answer = x * y;
+    answer = firstNum * secNum;
   } else if (operator == "/") {
-    answer = x / y;
+    answer = firstNum / secNum;
   }
   return answer;
 };
@@ -15,18 +15,27 @@ window.onload = function() {
   var calculator = document.getElementById("calculator");
   var answerDiv  = document.getElementById("answer");
 
-  calculator.addEventListener("click", function (event){
+  calculator.addEventListener("submit", function (event){
   event.preventDefault();
-  var x         = parseFloat(document.getElementById("firstNum").value);
-  var y         = parseFloat(document.getElementById("secNum").value);
-  var operator  = document.getElementById("operator").value;
+  var firstNum  = parseFloat(calculator.elements.firstNum.value);
+  var secNum    = parseFloat(calculator.elements.secNum.value);
+  var operator  = calculator.elements.operator.value;
 
-  var answer    = calculate(x, operator, y);
-  calculator.elements.x.value         = answer;
+  var answer    = calculate(firstNum, operator, secNum);
+  calculator.elements.firstNum.value         = answer;
   answerDiv.innerHTML                 = answer;
   });
 
   select.addEventListener("change", function (){
 
+  });
+
+  var clear = document.getElementById("clear");
+  clear.addEventListener("click", function (event) {
+    event.preventDefault();
+    calculator.elements.firstNum.value = null;
+    calculator.elements.secNum.value   = null;
+    answerDiv.innerHTML = 0;
+    calculator.elements.firstNum.focus();
   });
 };
