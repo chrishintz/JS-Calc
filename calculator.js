649be141770db83b02c6ed9bd0b99f41b45dc6a1
Chris Hintz <chrishintz42@gmail.com>
2015-12-15 00:16:25 -0800

    Clear button working.  Styling next

--- calculator.js
+++ calculator.js
@@ -1,12 +1,12 @@
-var calculate = function (firstNum, operator, secNum) {
-  if (operator =="+") {
-    answer = firstNum + secNum;
+var calculate = function (firstNum, operator, secondNum) {
+  if (operator        == "+") {
+    answer = firstNum + secondNum;
   } else if (operator == "-") {
-    answer = firstNum - secNum;
+    answer = firstNum - secondNum;
   } else if (operator == "x") {
-    answer = firstNum * secNum;
+    answer = firstNum * secondNum;
   } else if (operator == "/") {
-    answer = firstNum / secNum;
+    answer = firstNum / secondNum;
   }
   return answer;
 };
@@ -17,25 +17,21 @@ window.onload = function() {
 
   calculator.addEventListener("submit", function (event){
   event.preventDefault();
-  var firstNum  = parseFloat(calculator.elements.firstNum.value);
-  var secNum    = parseFloat(calculator.elements.secNum.value);
-  var operator  = calculator.elements.operator.value;
+  var firstNum         = parseFloat(calculator.elements.firstNum.value);
+  var secondNum        = parseFloat(calculator.elements.secondNum.value);
+  var operator         = calculator.elements.operator.value;
 
-  var answer    = calculate(firstNum, operator, secNum);
-  calculator.elements.firstNum.value         = answer;
+  var answer    = calculate(firstNum, operator, secondNum);
+  calculator.elements.firstNum.value  = answer;
   answerDiv.innerHTML                 = answer;
-  });
-
-  select.addEventListener("change", function (){
-
   });
 
   var clear = document.getElementById("clear");
   clear.addEventListener("click", function (event) {
     event.preventDefault();
-    calculator.elements.firstNum.value = null;
-    calculator.elements.secNum.value   = null;
-    answerDiv.innerHTML = 0;
+    calculator.elements.firstNum.value    = null;
+    calculator.elements.secondNum.value   = null;
+    answerDiv.innerHTML                   = 0;
     calculator.elements.firstNum.focus();
   });
 };
